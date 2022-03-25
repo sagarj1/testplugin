2cf8112e8240cda7d65044a692610ef6526b57d1
GitHub Action <action@github.com>
2022-03-25 09:39:58 +0000

    Template cleanup

--- src/main/kotlin/com/github/sagarj1/testplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/sagarj1/testplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.sagarj1.testplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.sagarj1.testplugin.MyBundle
 
 class MyApplicationService {
 
